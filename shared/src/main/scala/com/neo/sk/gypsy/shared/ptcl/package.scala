35e19fde1d9453bb462d0cf388b648e8a2d8606f
吴艺萌 <wuyimeng@neotel.com.cn>
2018-07-23 11:56:24 +0800

    debug

--- shared/src/main/scala/com/neo/sk/gypsy/shared/ptcl/package.scala
+++ shared/src/main/scala/com/neo/sk/gypsy/shared/ptcl/package.scala
@@ -63,7 +63,7 @@ package object ptcl {
                  id:Long,
                  x:Int,
                  y:Int,
-                 mass:Double = 300,
+                 mass:Double = 10,
                  radius:Double = 4 + sqrt(10)*6,
                  speed:Double = 12
                  )
