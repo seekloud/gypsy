1e4bbeeaa42dd118d59ad529c2dabb5466860179
曹慎凯 <caoshenkai@neotel.com.cn>
2018-09-20 16:08:41 +0800

    限时模式

--- backend/src/main/scala/com/neo/sk/gypsy/core/RoomActor.scala
+++ backend/src/main/scala/com/neo/sk/gypsy/core/RoomActor.scala
@@ -172,7 +172,7 @@ object RoomActor {
             dispatchTo(subscribersMap,p._1,WsMsgProtocol.GameOverMessage(p._1,p._2.kill,p._2.cells.map(_.mass).sum.toInt,overTime-p._2.startTime))
           }
           timer.cancel(SyncTimeKey)
-          Behaviors.same
+          Behaviors.stopped
         case x =>
           log.warn(s"got unknown msg: $x")
           Behaviors.unhandled
