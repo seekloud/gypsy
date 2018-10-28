125d7b6eeb5b5f3bc05d03b53ce5f48d84c62652
zhaoyin <zhaoyin@neotel.com.cn>
2018-10-28 14:51:10 +0800

    客户端-01

--- client/src/main/scala/com/neo/sk/gypsy/ClientBoot.scala
+++ client/src/main/scala/com/neo/sk/gypsy/ClientBoot.scala
@@ -1,13 +1,21 @@
 package com.neo.sk.gypsy
 
 import akka.actor.{ActorSystem, Scheduler}
+import akka.stream.ActorMaterializer
+import akka.actor.typed.scaladsl.adapter._
+import javafx.application.{Application, Platform}
+import com.neo.sk.gypsy.common.AppSettings._
 
 /**
   * @author zhaoyin
   * @date 2018/10/28  2:45 PM
   */
 
-
+object ClientBoot{
+  implicit val system = ActorSystem("gypsy",config)
+}
 class ClientBoot {
 
 }
+
+
