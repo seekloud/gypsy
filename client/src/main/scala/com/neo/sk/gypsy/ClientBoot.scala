35b0312e756ecd6bed0eed3fcdc89a061010b8c3
zhaoyin <zhaoyin@neotel.com.cn>
2018-10-28 16:16:57 +0800

    客户端-02 websocket链接

--- client/src/main/scala/com/neo/sk/gypsy/ClientBoot.scala
+++ client/src/main/scala/com/neo/sk/gypsy/ClientBoot.scala
@@ -1,9 +1,12 @@
 package com.neo.sk.gypsy
 
+import javafx.stage.Stage
 import akka.actor.{ActorSystem, Scheduler}
 import akka.stream.ActorMaterializer
 import akka.actor.typed.scaladsl.adapter._
 import javafx.application.{Application, Platform}
+
+
 import com.neo.sk.gypsy.common.AppSettings._
 
 /**
@@ -13,9 +16,21 @@ import com.neo.sk.gypsy.common.AppSettings._
 
 object ClientBoot{
   implicit val system = ActorSystem("gypsy",config)
-}
-class ClientBoot {
+  implicit val executor = system.dispatchers.lookup("akka.actor.my-blocking-dispatcher")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val scheduler: Scheduler = system.scheduler
 
+  val gameHolderClient = system.spawn(GameHolderClient.create(),"gameHolder")
+
+
+
+}
+class ClientBoot extends javafx.application.Application{
+
+  import ClientBoot._
+  override def start(mainStage: Stage): Unit = {
+    val wsClient = system.spawn(WsClient.create(),"WsClient")
+  }
 }
 
 
