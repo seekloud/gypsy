a18ffc942a3d9db3a7d7514bdffcc2ce65d9849d
zhaoyin <zhaoyin@neotel.com.cn>
2018-11-05 18:06:24 +0800

    回放新增播放完成

--- shared/src/main/scala/com/neo/sk/gypsy/shared/ptcl/Protocol.scala
+++ shared/src/main/scala/com/neo/sk/gypsy/shared/ptcl/Protocol.scala
@@ -118,13 +118,13 @@ object Protocol {
   case class MouseMove(userId:String,direct:(Double,Double), override val frame:Long, override val serialNum:Int) extends GameEvent
   case class KeyPress(userId:String,keyCode: Int, override val frame:Long, override val serialNum:Int) extends GameEvent
 
-   case class GenerateApples(apples:Map[Point, Int], override val frame:Long) extends GameEvent
-   case class RemoveApples(apples:Map[Point, Int], override val frame:Long) extends GameEvent
-   case class GenerateVirus(virus: Map[Long,Virus], override val frame:Long) extends GameEvent with WsMsgSource
-   case class RemoveVirus(virus: Map[Long,Virus], override val frame:Long) extends GameEvent
-   case class GenerateMass(massList:List[Mass], override val frame:Long) extends GameEvent
-   case class RemoveMass(massList:List[Mass], override val frame:Long) extends GameEvent with WsMsgSource
-   case class ReduceApples(apples:List[Food], override val frame:Long) extends GameEvent
+  case class GenerateApples(apples:Map[Point, Int], override val frame:Long) extends GameEvent
+  case class RemoveApples(apples:Map[Point, Int], override val frame:Long) extends GameEvent
+  case class GenerateVirus(virus: Map[Long,Virus], override val frame:Long) extends GameEvent with WsMsgSource
+  case class RemoveVirus(virus: Map[Long,Virus], override val frame:Long) extends GameEvent
+  case class GenerateMass(massList:List[Mass], override val frame:Long) extends GameEvent
+  case class RemoveMass(massList:List[Mass], override val frame:Long) extends GameEvent with WsMsgSource
+  case class ReduceApples(apples:List[Food], override val frame:Long) extends GameEvent
   case class ReduceVirus(apples:List[Food], override val frame:Long) extends GameEvent
   case class PlayerInfoChange(player: Map[String,Player], override val frame:Long) extends GameEvent
   //  缩放放到
