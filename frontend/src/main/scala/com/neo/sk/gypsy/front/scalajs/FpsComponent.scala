d2ac7411fbcc7605c02333c0cf69deec7d52d8e9
zhaoyin <zhaoyin@neotel.com.cn>
2018-11-15 10:46:02 +0800

    修复screeScale为0的bug

--- frontend/src/main/scala/com/neo/sk/gypsy/front/scalajs/FpsComponent.scala
+++ frontend/src/main/scala/com/neo/sk/gypsy/front/scalajs/FpsComponent.scala
@@ -3,6 +3,7 @@ package com.neo.sk.gypsy.front.scalajs
 
 import org.scalajs.dom
 import org.scalajs.dom.ext.Color
+import com.neo.sk.gypsy.shared.ptcl._
 
 /**
   * Created by hongruying on 2018/8/29
@@ -25,24 +26,25 @@ object FpsComponent{
     }
   }
 
-   def renderFps(ctx:dom.CanvasRenderingContext2D,ping:Long) = {
+   def renderFps(ctx:dom.CanvasRenderingContext2D,ping:Long,newWindowX:Int) = {
     addFps()
     if(isRenderFps){
-      ctx.font = "20px Helvetica"
+      ctx.font = s"${ 20 * newWindowX / Window.w }px Helvetica"
       ctx.fillStyle = Color.White.toString()
       val fpsString = "fps : "
       val pingString = "ping: "
-      ctx.fillText(fpsString, 750, 30)
-      ctx.fillText(pingString, 750 + ctx.measureText(fpsString).width + 50, 30)
+      ctx.fillText(fpsString, newWindowX * 0.6, 30)
+      ctx.fillText(pingString, newWindowX * 0.6 + ctx.measureText(fpsString).width + 50, 30)
+      //fps
       ctx.strokeStyle = "black"
-      ctx.strokeText(lastRenderTimes.toString, 750 + ctx.measureText(fpsString).width, 30)
+      ctx.strokeText(lastRenderTimes.toString, newWindowX * 0.6 + ctx.measureText(fpsString).width, 30)
       ctx.fillStyle = if (lastRenderTime < 50) Color.Red.toString() else Color.Green.toString()
-      ctx.fillText(lastRenderTimes.toString, 750 + ctx.measureText(fpsString).width, 30)
+      ctx.fillText(lastRenderTimes.toString, newWindowX * 0.6 + ctx.measureText(fpsString).width, 30)
+      //ping
       ctx.strokeStyle = "black"
-      ctx.strokeText(s"${ping}ms", 750 + ctx.measureText(fpsString).width + ctx.measureText(pingString).width + 60, 30)
+      ctx.strokeText(s"${ping}ms", newWindowX * 0.6 + ctx.measureText(fpsString).width + ctx.measureText(pingString).width + 60, 30)
       ctx.fillStyle = if (ping <= 100) Color.Green.toString() else if (ping > 100 && ping <= 200) Color.Yellow.toString() else Color.Red.toString()
-      ctx.fillText(s"${ping}ms", 750 + ctx.measureText(fpsString).width + ctx.measureText(pingString).width + 60, 30)
-      //      ctx.fillText(s"ping: ${networkLatency}ms",canvasBoundary.x * canvasUnit - ctx.measureText(),(canvasBoundary.y - LittleMap.h - 2) * canvasUnit,10 * canvasUnit)
+      ctx.fillText(s"${ping}ms", newWindowX * 0.6 + ctx.measureText(fpsString).width + ctx.measureText(pingString).width + 60, 30)
     }
   }
 
