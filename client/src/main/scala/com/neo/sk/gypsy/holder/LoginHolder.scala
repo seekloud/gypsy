6bbf834039755f3dd10490c1252884710ca989ed
zhaoyin <zhaoyin@neotel.com.cn>
2018-11-19 23:36:43 +0800

    客户端二维码

--- client/src/main/scala/com/neo/sk/gypsy/holder/LoginHolder.scala
+++ client/src/main/scala/com/neo/sk/gypsy/holder/LoginHolder.scala
@@ -5,17 +5,30 @@ import com.neo.sk.gypsy.ClientBoot
 import com.neo.sk.gypsy.actor.WsClient
 import com.neo.sk.gypsy.common.StageContext
 import com.neo.sk.gypsy.scene.LoginScene
+import com.neo.sk.gypsy.common.Api4GameAgent._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
   * @author zhaoyin
-  * @date 2018/10/29  5:14 PM
+  * 2018/10/29  5:14 PM
   */
 class LoginHolder(
                    wsClient: ActorRef[WsClient.WsCommand],
                    loginScene: LoginScene,
                    stageCtx: StageContext
                  ) {
+  var wsUrl = ""
+  var scanUrl = ""
   loginScene.setLoginSceneListener(new LoginScene.LoginSceneListener {
     override def onButtonConnect(): Unit = {
+      getLoginResponseFromEs().map {
+        case Right(r) =>
+          wsUrl = r.data.wsUrl
+          scanUrl = r.data.scanUrl
+          //TODO
+        case Left(l) =>
+      }
+
       val id = System.currentTimeMillis().toString
       val name = "name" + System.currentTimeMillis().toString
       val accessCode = "jgfkldpwer"
