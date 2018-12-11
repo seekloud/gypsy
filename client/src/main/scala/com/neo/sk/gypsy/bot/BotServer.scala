c46573a162eb35c5e3865ef052c53899336c5b37
zhaoyin <zhaoyin@neotel.com.cn>
2018-12-11 23:04:38 +0800

    botServer+botActor+botHolder

--- client/src/main/scala/com/neo/sk/gypsy/bot/BotServer.scala
+++ client/src/main/scala/com/neo/sk/gypsy/bot/BotServer.scala
@@ -28,23 +28,36 @@ object EsheepServer {
 class EsheepService(botActor:ActorRef[BotActor.Command]) extends EsheepAgent {
   override def createRoom(request: Credit): Future[CreateRoomRsp] = {
     println(s"createRoom Called by [$request")
-    botActor ! CreateRoom(request.playerId,request.apiToken)
-    val state = State.init_game
-    Future.successful(CreateRoomRsp(errCode = 101, state = state, msg = "ok"))
+    if(checkBotToken(request.playerId,request.apiToken)){
+      botActor ! CreateRoom(request.playerId,request.apiToken)
+      val state = State.init_game
+      Future.successful(CreateRoomRsp(errCode = 101, state = state, msg = "ok"))
+    }else{
+      Future.successful(CreateRoomRsp(errCode = 100002, state = State.unknown, msg = "auth error")))
+    }
   }
 
   override def joinRoom(request: JoinRoomReq): Future[SimpleRsp] = {
     println(s"joinRoom Called by [$request")
-    botActor ! JoinRoom(request.roomId,request.credit.get.playerId,request.credit.get.apiToken)
-    val state = State.in_game
-    Future.successful(SimpleRsp(errCode = 102, state = state, msg = "ok"))
+    if(checkBotToken(request.credit.get.playerId,request.credit.get.apiToken)){
+      botActor ! JoinRoom(request.roomId,request.credit.get.playerId,request.credit.get.apiToken)
+      val state = State.in_game
+      Future.successful(SimpleRsp(errCode = 102, state = state, msg = "ok"))
+    }else{
+      Future.successful(SimpleRsp(errCode = 100002, state = State.unknown, msg = "auth error"))
+    }
+
   }
 
   override def leaveRoom(request: Credit): Future[SimpleRsp] = {
     println(s"leaveRoom Called by [$request")
-    botActor ! LeaveRoom(request.playerId)
-    val state = State.ended
-    Future.successful(SimpleRsp(errCode = 103, state = state, msg = "ok"))
+    if(checkBotToken(request.playerId,request.apiToken)){
+      botActor ! LeaveRoom(request.playerId)
+      val state = State.ended
+      Future.successful(SimpleRsp(errCode = 103, state = state, msg = "ok"))
+    }else{
+      Future.successful(SimpleRsp(errCode = 100002, state = State.unknown, msg = "auth error"))
+    }
   }
 
   override def actionSpace(request: Credit): Future[ActionSpaceRsp] = {
@@ -70,14 +83,20 @@ class EsheepService(botActor:ActorRef[BotActor.Command]) extends EsheepAgent {
 
   override def observation(request: Credit): Future[ObservationRsp] = {
     println(s"action Called by [$request")
-    val rsp = ObservationRsp()
-    Future.successful(rsp)
+    if(checkBotToken(request.playerId,request.apiToken)){
+      //TODO
+      val rsp = ObservationRsp()
+      Future.successful(rsp)
+    }else{
+      Future.successful(ObservationRsp(errCode = 100002, state = State.unknown, msg = "auth error"))
+    }
   }
 
   override def inform(request: Credit): Future[InformRsp] = {
     println(s"action Called by [$request")
     if(checkBotToken(request.playerId,request.apiToken)){
-      val rsp = InformRsp(score = botHolder.getInform.)
+      val rsp = InformRsp(score = botHolder.getInform(request.playerId)._1.toInt, kills = botHolder.getInform(request.playerId)._2,
+        state = State.unknown, msg = "ok")
       Future.successful(rsp)
     }else{
       Future.successful(InformRsp(errCode = 100002, state = State.unknown, msg = "auth error"))
