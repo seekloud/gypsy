7f7d7a46b5cc319da4f2dde43107a57d217d4f8d
YouSei <yangxingyuan@neotel.com.cn>
2019-01-22 11:37:33 +0800

    test

--- shared/src/main/scala/com/neo/sk/gypsy/shared/ptcl/GameConfig.scala
+++ shared/src/main/scala/com/neo/sk/gypsy/shared/ptcl/GameConfig.scala
@@ -61,7 +61,7 @@ object GameConfig {
   val maxDelayFrame = 3
 
 //  初始质量
-  val initMass:Short = 6000
+  val initMass:Short = 10
 //病毒分裂个数
   val VirusSplitNumber = 13
 //  玩家初始长宽
@@ -84,7 +84,7 @@ object GameConfig {
   val CanvasHeight = 600
 
 //  胜利分数
-  val VictoryScore = 20000
+  val VictoryScore = 2000
 
 //  val KillBotScore = 20000
   val KillBotScore = VictoryScore/2
