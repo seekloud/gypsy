e7eec48e6567c2f904763509e616383fd54efc0a
xusiran <xusiran@neotel.com.cn>
2019-03-08 15:11:41 +0800

    botServer build

--- client/src/main/scala/com/neo/sk/gypsy/actor/SdkServer.scala
+++ client/src/main/scala/com/neo/sk/gypsy/actor/SdkServer.scala
@@ -2,8 +2,10 @@ package com.neo.sk.gypsy.actor
 
 import akka.actor.typed._
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors, StashBuffer, TimerScheduler}
+import com.neo.sk.gypsy.botService.BotServer
 import io.grpc.Server
 import org.slf4j.LoggerFactory
+
 import scala.concurrent.ExecutionContext
 /**
   * create by zhaoyin
@@ -16,6 +18,10 @@ object SdkServer {
   private val log = LoggerFactory.getLogger("sdkserver")
   case object Shutdown extends Command
 
+  case class BuildServer(port: Int,
+    executionContext: ExecutionContext
+    ) extends Command
+
 
   private[this] def switchBehavior(ctx: ActorContext[Command],
                                    behaviorName: String,
@@ -25,24 +31,35 @@ object SdkServer {
     stashBuffer.unstashAll(ctx, behavior)
   }
 
-  def create(): Behavior[Command] = {
+  def create(botActor: ActorRef[BotActor.Command]): Behavior[Command] = {
     Behaviors.setup[Command] { ctx =>
       Behaviors.withTimers[Command] { implicit timer =>
         implicit val stashBuffer: StashBuffer[Command] = StashBuffer[Command](Int.MaxValue)
 
-        switchBehavior(ctx, "idle", idle())
+        switchBehavior(ctx, "idle", idle(botActor))
       }
     }
   }
 
-  private def idle()
+  private def idle(botActor: ActorRef[BotActor.Command])
                   (implicit stashBuffer: StashBuffer[Command],
                    timer: TimerScheduler[Command]
                   ): Behavior[Command] = {
     Behaviors.receive { (ctx, msg) =>
       msg match {
-        case BuildServer(port, executor, act,gController, gameMessageReceiver, stageCtx) =>
-          //TODO 启动BotServer服务
+        case BuildServer(port, executor) =>
+          //FIXME 启动BotServer服务
+          val port = 5321
+          val server = BotServer.build(port, executor, botActor)
+          server.start()
+          log.debug(s"Server started at $port")
+          sys.addShutdownHook {
+            log.debug("JVM SHUT DOWN.")
+            server.shutdown()
+            log.debug("SHUT DOWN.")
+          }
+          //          server.awaitTermination()
+          //          log.debug("DONE.")
           working(server)
       }
     }
