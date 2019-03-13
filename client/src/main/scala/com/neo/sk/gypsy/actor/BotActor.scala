eee3c7672d11c6ff7db8ee4bb532d9bbd361fb54
zhaoyin <zhaoyin@neotel.com.cn>
2019-03-13 22:18:33 +0800

    bot joinroom

--- client/src/main/scala/com/neo/sk/gypsy/actor/BotActor.scala
+++ client/src/main/scala/com/neo/sk/gypsy/actor/BotActor.scala
@@ -148,9 +148,8 @@ object BotActor {
           waitingGame(gameClient,stageCtx,stream)
 
         case ClientTest(roomId)=>
-          log.info("get clientTest")
-//          val rsp = botClient.joinRoom("1","")
-          val rsp = botClient.createRoom("")
+          val rsp = botClient.joinRoom("1","")
+//          val rsp = botClient.createRoom("")
           Behaviors.same
 
         case unknown@_ =>
@@ -168,9 +167,8 @@ object BotActor {
       msg match {
 
         case ClientTest(roomId)=>
-          log.info("get clientTest")
-//          val rsp = botClient.joinRoom("1","")
-          val rsp = botClient.createRoom("")
+          val rsp = botClient.joinRoom("1","")
+//          val rsp = botClient.createRoom("")
           Behaviors.same
 
         case CreateRoom(sender) =>
